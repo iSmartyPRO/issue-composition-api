3ed7b6f60a73269d5a87590aa07906a8cc423d49
Ilias Aidar <ilias.aidar@gencoindustry.com>
2021-12-03 17:10:02 +0300

    Issue resolved, thanks for help

--- src/components/form-avatar-composition.vue
+++ src/components/form-avatar-composition.vue
@@ -17,25 +17,19 @@ import { ref } from '@vue/reactivity'
 export default {
   name: "FormAvatarComposition",
   setup() {
-
-    const dialogAvatar = ref({}) // объявил константу, которая ссылается на dialogAvatar, нужен ли он? либо надо как-то по другому его объявлять?
-
+    const dialogAvatar = ref()
     const showFileChooser = () => {
-      // dialogAvatar.click() // Этот вариант не работает, в консоли пишет > Uncaught TypeError: dialogAvatar.click is not a function
-      // console.log(dialogAvatar.value) // в консоле пишет undefined
-
-      // кто знает, как реализовать подобный механизм как в Standard? Прошу подедиться опытом.
+      console.log(dialogAvatar.value)
+      dialogAvatar.value.click()
     }
-
     const setImage = () => {
-      alert('Something amazing haapened from composition API )')
+      alert('Something amazing happened from composition API )')
     }
-
     return {
+      dialogAvatar,
       showFileChooser,
       setImage
     }
-
   }
 
   // не надо использовать methods, только через setup()
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
+    <p>Status: RESOLVED! )</p>
   </div>
 </template>
 
